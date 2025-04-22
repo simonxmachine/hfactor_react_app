758515f72049ad62bac777ab725cad1fae40f3d6
System Administrator <simonxmachine@gmail.com>
2025-04-21 23:33:49 -0400

    updated homequote and lint error

--- src/components/HomeFirstQuote.tsx
+++ src/components/HomeFirstQuote.tsx
@@ -5,28 +5,48 @@ export default function HomeFirstQuote() {
     <section className={styles.quoteSection}>
       <div className={styles.content}>
         <blockquote className={styles.quote}>
-          <span className={styles.quoteMarks}>"</span>I love H-Factor, one of
-          the few purified waters infused with hydrogen.
-          <span className={styles.quoteMarks}>"</span>
+          <span className={styles.quoteMarks}></span>An{" "}
+          <span className={styles.award}>award-winning</span> hydrogen infused
+          water that offers anti-inflammatory and antioxidant benefits.
+          <span className={styles.quoteMarks}></span>
         </blockquote>
         <div className={styles.logos}>
-          <img src="/logos/forbes.png" alt="Forbes" className={styles.logo} />
           <img
-            src="/logos/business-insider.png"
-            alt="Business Insider"
+            src="/store_brands/walmart.webp"
+            alt="Walmart"
+            className={styles.logo}
+          />
+          <img src="/store_brands/cvs.png" alt="CVS" className={styles.logo} />
+          <img
+            src="/store_brands/sprouts.png"
+            alt="Sprouts"
             className={styles.logo}
           />
           <img
-            src="/logos/la-times.png"
-            alt="Los Angeles Times"
+            src="/store_brands/stop_shop.png"
+            alt="Stop & Shop"
             className={styles.logo}
           />
           <img
-            src="/logos/yahoo-finance.png"
-            alt="Yahoo Finance"
+            src="/store_brands/winn_dixie.png"
+            alt="Winn Dixie"
             className={styles.logo}
           />
-          <img src="/logos/bevnet.png" alt="BevNET" className={styles.logo} />
+          <img
+            src="/store_brands/bristol_farms.png"
+            alt="Bristol Farms"
+            className={styles.logo}
+          />
+          <img
+            src="/store_brands/acme.png"
+            alt="Acme"
+            className={styles.logo}
+          />
+          <img
+            src="/store_brands/rouses.png"
+            alt="Rouses"
+            className={styles.logo}
+          />
         </div>
       </div>
       <div className={styles.wave}>
